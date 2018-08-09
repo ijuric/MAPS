09604cea68fdde75cd5898463a2b453182adad68
Armen Abnousi <a.abnousi@outlook.com>
2018-08-09 07:51:26 -0400

    combining script added; similar to run_pipeline

---
+++ bin/combine_datasets.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+python_path=/home/abnousa/software/python3.6.5/bin/python #should have pysam, pybedtools installed. bedtools, samtools should be in the path
+Rscript_path=/opt/R-3.4.3/lib64/R/bin/Rscript
+###################################################################
+feather=1 #start from feather or run only MAPS
+maps=0
+number_of_datasets=1
+dataset_name="test"
+outdir="/home/jurici/MAPS/PLAC-Seq_datasets/test_dataset2"
+macs2_filepath="/home/jurici/MAPS/PLAC-Seq_datasets/test_dataset2/MACS2_peaks/final.replicated.narrowPeak"
+organism="mm10"
+bin_size=5000
+filter_file="None"
+generate_hic=1
+####################################################################
+### specify as many datasets as required
+####################################################################
+dataset1="/home/jurici/MAPS/PLAC-Seq_datasets/RenLab_F123_CTCF/feather_output/CTCF.rep1_current"
+dataset2="/home/jurici/MAPS/PLAC-Seq_datasets/RenLab_F123_CTCF/feather_output/CTCF.rep2_current"
+dataset3=""
+dataset4=""
+#...
+###################################################################
+
+DATE=`date '+%Y%m%d_%H%M%S'`
+#####Armen:
+fastq1=$fastq_dir/$dataset_name"_R1"$fastq_format
+fastq2=$fastq_dir/$dataset_name"_R2"$fastq_format
+feather_output=$outdir"/feather_output/"$dataset_name"_"$DATE
+feather_output_symlink=$outdir"/feather_output/"$dataset_name"_current"
+resolution=$(bc <<< "$bin_size/1000")
+per_chr='True' # set this to zero if you don't want per chromosome output bed and bedpe files
+feather_logfile=$feather_output"/"$dataset_name".feather.log"
+resolution=$(bc <<< "$bin_size/1000")
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+hic_dir="tempfiles/hic_tempfiles"
+if [ $organism == "mm10" ]; then
+	if [ -z $bwa_index ]; then
+        	bwa_index="/home/jurici/MAPS/MAPS_data_files/"$organism"/BWA_index/mm10_chrAll.fa"
+	fi
+	genomic_feat_filepath=$cwd"/../MAPS_data_files/"$organism"/genomic_features/F_GC_M_MboI_"$resolution"Kb_el.mm10.txt"
+	chr_count=19
+elif [ $organism == "mm9" ]; then
+	if [ -z $bwa_index ]; then
+		bwa_index="/home/jurici/MAPS/MAPS_data_files/"$organism"/BWA_index/mm9.fa"
+	fi
+	genomic_feat_filepath=$cwd"/../MAPS_data_files/"$organism"/genomic_features/F_GC_M_MboI_"$resolution"Kb_el.mm9.txt"
+	chr_count=19
+elif [ $organism == "hg19" ]; then
+	if [ -z $bwa_index ]; then
+		bwa_index="/home/jurici/MAPS/MAPS_data_files/"$organism"/BWA_index/hg19.fa"
+	fi
+	genomic_feat_filepath=$cwd"/../MAPS_data_files/"$organism"/genomic_features/F_GC_M_MboI_"$resolution"Kb_el.hg19.txt"
+	chr_count=22
+elif [ $organism == "hg38" ]; then
+	if [ -z $bwa_index ]; then
+		bwa_index="/home/jurici/MAPS/MAPS_data_files/"$organism"/BWA_index/hg38.fa"
+	fi
+	genomic_feat_filepath=$cwd"/../MAPS_data_files/"$organism"/genomic_features/F_GC_M_MboI_"$resolution"Kb_el.GRCh38.txt"
+	chr_count=22
+fi
+
+####Ivan:"
+long_bedpe_dir=$feather_output_symlink"/"
+short_bed_dir=$feather_output_symlink"/"
+maps_output=$outdir"/MAPS_output/"$dataset_name"_"$DATE"/"
+maps_output_symlink=$outdir"/MAPS_output/"$dataset_name"_current"
+#genomic_feat_filepath="/home/jurici/MAPS/MAPS_data_files/"$organism"/genomic_features/"$genomic_features_filename
+
+if [ $feather -eq 1 ]; then
+	mkdir -p $feather_output
+	if [ $number_of_datasets -ge 2 ]; then
+		ds_array=()
+		hic_array=()
+		qc_array=()
+		for i in `seq $number_of_datasets`
+		do
+			ds_name="dataset$i"
+			hic_name="$ds_name/$hic_dir"
+			#printf "$dataset1"
+			#printf "$ds_name\n"
+			eval ds=\$$ds_name
+			eval hic=\$$hic_name
+			qc_file=$(ls $ds/*.qc.tsv)
+			#printf "$ds\n"
+			ds_array+=($ds)
+			hic_array+=($hic)
+			qc_array+=($qc_file)
+			#printf "$i\n"
+			#printf '%s\n' "${ds_array[@]}"
+			#printf '%s\n' "${hic_array[@]}"
+			printf '%s\n' "${qc_array[@]}"
+		done
+		$cwd/feather/concat_bedfiles.sh $feather_output $dataset_name "${ds_array[@]}"
+		qc_filename=$feather_output/$dataset_name".feather.qc"
+		awk '{a[FNR]=$1; b[FNR]+=$2; c[FNR]=$3} END{for (i=1; i<=FNR; i++) print a[i], b[i], c[i]}' "${qc_array[@]}" > $qc_filename".tsv"
+		sed -i 's/ /\t/g' $qc_filename".tsv"
+		if [ $generate_hic -eq 1 ]; then
+			echo "$feather_output/$hic_dir"
+			mkdir -p $feather_output"/"$hic_dir
+			$cwd/feather/concat_hic.sh  $feather_output $dataset_name $hic_dir "${hic_array[@]}"
+		fi
+	else
+		$python_path $cwd/feather/feather_pipe preprocess -o $feather_output -p $dataset_name -f1 $fastq1 -f2 $fastq2 -b $bwa_index -q $mapq -l $length_cutoff -t $threads -c $per_chr -j $generate_hic
+		qc_filename=$feather_output/$dataset_name".feather.qc"
+		temp_qc_file=$feather_output/tempfiles/$dataset_name".feather.qc.modified"
+		#printf "dataset name:\t"$dataset_name"\n" >> $qc_filename
+		#printf "MACS2 file:\t"$macs2_filename"\n" >> $qc_filename
+		sed -r 's/  +/\t/g' $qc_filename > $temp_qc_file
+		sed -r -i 's/ /\_/g' $temp_qc_file
+		cut -f 1-2 $temp_qc_file > $temp_qc_file".cut"
+		sed -i 's/\_$//g' $temp_qc_file".cut"
+		paste -d"\t" $cwd/feather/qc_template.txt $temp_qc_file".cut" > $temp_qc_file".cut.tmp"
+		awk '{print $1,$3,$2}' $temp_qc_file".cut.tmp" >> $qc_filename".tsv"
+		sed -i 's/ /\t/g' $qc_filename".tsv"
+		#awk '{printf("%s\t", $1)}' $temp_qc_file".cut" > $qc_filename".tsv"
+		#printf "\n" >> $qc_filename".tsv"
+		#awk '{printf("%s\t", $2)}' $temp_qc_file".cut" >> $qc_filename".tsv"
+	fi
+	cp "$(readlink -f $0)" $feather_output"/execution_script_copy"
+	chmod 777 $feather_output
+	ln -sfn $feather_output $feather_output_symlink
+fi
+	
+if [ $maps -eq 1 ]; then
+	mkdir -p $maps_output
+	echo "$dataset_name $maps_output $macs2_filepath $genomic_feat_filepath $long_bedpe_dir $short_bed_dir $bin_size $chr_count $maps_output"
+	$python_path $cwd/MAPS/make_maps_runfile.py $dataset_name $maps_output $macs2_filepath $genomic_feat_filepath $long_bedpe_dir $short_bed_dir $bin_size $chr_count $maps_output
+	echo "first"
+	$python_path $cwd/MAPS/MAPS.py $maps_output"maps_"$dataset_name".maps"
+	echo "second"
+	$Rscript_path $cwd/MAPS/MAPS_regression_and_peak_caller.r $maps_output $dataset_name"."$resolution"k" $bin_size $chr_count $filter_file
+	echo "third"
+	cp "$(readlink -f $0)" $maps_output"/execution_script_copy"
+	chmod 777 $maps_output
+	ln -sfn $maps_output $maps_output_symlink
+fi
